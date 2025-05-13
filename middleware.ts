be679fe3f20715d1f53080e5b8227430174d636c
Mindsetofchampions <132849068+Mindsetofchampions@users.noreply.github.com>
2025-05-13 12:39:15 -0400

    Temporarily disable Clerk authentication

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,5 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  // Public routes that don't require authentication
-  publicRoutes: [
-    "/",
-    "/pricing",
-    "/videos",
-    "/videos/(.*)",
-    "/sign-in",
-    "/sign-up",
-    "/api/webhooks/(.*)",
-    "/creators",
-    "/creators/(.*)",
-    "/corporate",
-    "/corporate/(.*)",
-    "/sponsors",
-    "/shop",
-    "/advertise",
-    "/shows",
-    "/shows/(.*)",
-    "/live"
-  ],
-  
-  // Routes that can be accessed without authentication
-  ignoredRoutes: [
-    "/api/webhooks/(.*)"
-  ],
-});
- 
+export default function middleware() {}
+
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)"
-  ],
+  matcher: [],
 };
